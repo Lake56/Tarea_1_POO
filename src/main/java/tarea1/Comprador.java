f3f4f877d609a86df25f09cfd5b80654999e606c
mtn928 <mcarrasco2025@inf.udec.cl>
2026-04-22 15:38:59 -0400

    primercommit

---
+++ src/main/java/tarea1/Comprador.java
@@ -0,0 +1,19 @@
+package tarea1;
+
+public class Comprador {
+
+    private String sonido;
+    private int vuelto;
+
+    public Comprador(Moneda m, int cualProducto, Expendedor exp){
+
+    }
+
+    public int cuantoVuelto() {
+        return vuelto;
+    }
+
+    public String queBebiste() {
+        return sonido;
+    }
+}
---
+++ src/main/java/tarea1/Moneda.java
@@ -0,0 +1,40 @@
+package tarea1;
+
+//interfaz del comparable
+public abstract class Moneda implements Comparable{
+    //constructor
+    public Moneda() {}
+    //getters
+    public Moneda getSerie() {
+        return this;
+    }
+    public abstract int getValor();
+
+    public int compareTo(Moneda otra) {
+        return this.getValor()-otra.getValor();
+    }
+    //tostring
+    public String toString() {
+        return "Moneda: serie=" + hashCode() + ", valor=" + getValor() + "]";
+    }
+}
+//tipos de monedas
+class Moneda1500 extends Moneda {
+    public Moneda1500() { super(); }
+    public int getValor() { return 1500; }
+}
+
+class Moneda1000 extends Moneda {
+    public Moneda1000() { super(); }
+    public int getValor() { return 1000; }
+}
+
+class Moneda500 extends Moneda {
+    public Moneda500() { super(); }
+    public int getValor() { return 500; }
+}
+
+class Moneda100 extends Moneda {
+    public Moneda100() { super(); }
+    public int getValor() { return 100; }
+}
